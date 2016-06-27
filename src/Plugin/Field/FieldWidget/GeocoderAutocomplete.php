c00645f6b8839eb3d05e5d7bad87327554dbfe69
Stefan Auditor <stefan.auditor@erdfisch.de>
2016-06-27 16:31:44 +0200

    Remove unused user object

--- src/Plugin/Field/FieldWidget/GeocoderAutocomplete.php
+++ src/Plugin/Field/FieldWidget/GeocoderAutocomplete.php
@@ -33,13 +33,9 @@ class GeocoderAutocomplete extends StringTextfieldWidget {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $main_widget = parent::formElement($items, $delta, $element, $form, $form_state);
-
-    $user = \Drupal::currentUser();
-
     $element['value'] = $main_widget['value'];
 
     if (\Drupal::currentUser()->hasPermission('access geocoder autocomplete')) {
-
       $element['value'] = $element['value'] + array(
         '#size' => $this->getSetting('size'),
         '#autocomplete_route_name' => $this->getSetting('autocomplete_route_name'),
